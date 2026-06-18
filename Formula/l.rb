0e27feee427586b695a990f24dffae4c4ca52873
Devan Huapaya <huapayadevan@gmail.com>
2026-06-17 22:41:43 -0700

    Update l to v0.4.0

--- Formula/l.rb
+++ Formula/l.rb
@@ -1,26 +1,26 @@
 class L < Formula
   desc "an ls replacement"
   homepage "https://devan.gg/l"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/imdevan/l/releases/download/v0.3.0/l-darwin-amd64.tar.gz"
-      sha256 "0a7028207d53b8e2da582bf7616078cfaa97cbe3fa56b4c795a8cc32decb133c"
+      url "https://github.com/imdevan/l/releases/download/v0.4.0/l-darwin-amd64.tar.gz"
+      sha256 "a9c3f50f0d0d74b95dbff38d24812d63e7e5b54f14917daf4470d04aa50b7072"
     elsif Hardware::CPU.arm?
-      url "https://github.com/imdevan/l/releases/download/v0.3.0/l-darwin-arm64.tar.gz"
-      sha256 "946a98ab3e7489c25e5b60007ef5af6bc47654f363d377206da6179af90eb8cb"
+      url "https://github.com/imdevan/l/releases/download/v0.4.0/l-darwin-arm64.tar.gz"
+      sha256 "ffab14dce777425abb5c493e6db82b9622e88895ae2662cce47b93186315684a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/imdevan/l/releases/download/v0.3.0/l-linux-amd64.tar.gz"
-      sha256 "23d3d0dc7825a7b4cfb0db3bc244c619e4e210d5d526b1739b2f1ededffc823e"
+      url "https://github.com/imdevan/l/releases/download/v0.4.0/l-linux-amd64.tar.gz"
+      sha256 "db8c71b0143de814f6fa8fe827a8ac44dbff763a6f842775af5b02e6ef667afd"
     elsif Hardware::CPU.arm?
-      url "https://github.com/imdevan/l/releases/download/v0.3.0/l-linux-arm64.tar.gz"
-      sha256 "e72a8393aab5b007fce1f9672a5d84d5dbf3c60c0a9ca8cb9b6ad2440ea82925"
+      url "https://github.com/imdevan/l/releases/download/v0.4.0/l-linux-arm64.tar.gz"
+      sha256 "1de572f1d575c9128b79347bc8007fb019e6b83217d3f8d13f77617b153def17"
     end
   end
 
@@ -31,6 +31,6 @@ class L < Formula
   end
 
   test do
-    assert_match "v0.3.0", shell_output("#{bin}/l --version")
+    assert_match "v0.4.0", shell_output("#{bin}/l --version")
   end
 end
