38f7d0054b2d97f97e014b67d2105aaed789f8cb
Devan Huapaya <huapayadevan@gmail.com>
2026-06-17 22:33:57 -0700

    Update l to v0.3.0

--- Formula/l.rb
+++ Formula/l.rb
@@ -1,17 +1,34 @@
 class L < Formula
   desc "an ls replacement"
   homepage "https://devan.gg/l"
-  url "https://github.com/imdevan/l/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  version "0.3.0"
   license "MIT"
 
-  depends_on "go" => :build
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/imdevan/l/releases/download/v0.3.0/l-darwin-amd64.tar.gz"
+      sha256 "c6dfa9f3f17463704fbeeb5be82b8dbbceb6c9934d192a7d53bbca5c9ded967a"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/imdevan/l/releases/download/v0.3.0/l-darwin-arm64.tar.gz"
+      sha256 "8d66754b8799e64a8d9f4287fbd573fa17c75cfec336e907f19cfa49a17d151f"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/imdevan/l/releases/download/v0.3.0/l-linux-amd64.tar.gz"
+      sha256 "59ca517fc81cefe20dcba026fb46f7aa241d33f9bb8821e6e6b484b32b0e40c1"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/imdevan/l/releases/download/v0.3.0/l-linux-arm64.tar.gz"
+      sha256 "9ac8e26ff761b7900be9bca3ff2cfb006bbaf5766abf14cf56ae8116d79bedc2"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"l"), "./cmd/l"
+    bin.install "l"
   end
 
   test do
-    assert_match "v0.1.0", shell_output("#{bin}/l --version")
+    assert_match "v0.3.0", shell_output("#{bin}/l --version")
   end
 end
